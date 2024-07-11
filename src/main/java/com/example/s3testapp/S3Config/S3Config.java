6e2eae5486c41e9d54e936871d010f9ce9a56750
Minin81 <nikolai.minin81@gmail.com>
2024-07-11 21:46:31 +0300

    Initial commit

--- src/main/java/com/example/s3testapp/S3Config/S3Config.java
+++ src/main/java/com/example/s3testapp/S3Config/S3Config.java
@@ -7,17 +7,21 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration;
 
 @Configuration
 public class S3Config {
 
     private String accessKey = "DO0069DNU39VPYRNDMFE";
     private String accessSecret = "wzSlwUjXEsLKgUteCJBM3/8F99ZczErTb0lns5xIaZE";
-    private String region = "us-east-1";
+    private String spaceRegion = "us-east-1";
 
+    private String spaceEndpoint = "https://syd1.digitaloceanspaces.com";
     @Bean
-    public AmazonS3 S3Client() {
-        AWSCredentials credentials = new BasicAWSCredentials(accessKey, accessSecret);
-        return AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(credentials)).withRegion(region).build();
+    public AmazonS3 getS3() {
+        BasicAWSCredentials creds = new BasicAWSCredentials(accessKey, accessSecret);
+        return AmazonS3ClientBuilder.standard()
+                .withEndpointConfiguration(new EndpointConfiguration(spaceEndpoint, spaceRegion))
+                .withCredentials(new AWSStaticCredentialsProvider(creds)).build();
     }
 }
